b150498a49922c2656c0b7e7194d22117d6b1719
arpanjagdishbhairava <arpanraval17@gmail.com>
2024-02-28 20:02:13 -0500

    test case that checks the print of "Buzz" on multiple of 5 passes.

--- src/main/java/org/example/Fizzbuzz.java
+++ src/main/java/org/example/Fizzbuzz.java
@@ -10,6 +10,8 @@ public class Fizzbuzz {
     public static String fizzbuzz(int n) {
         if (n%3 == 0){
             return "Fizz";
+        } else if (n%5 == 0) {
+            return "Buzz";
         }
 
         return Integer.toString(n);
